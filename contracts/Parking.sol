6807f67accd0a5b536c2930166eeaff42fb6308a
dplamenov <dimitar.plamenov@gmail.com>
2022-04-22 12:21:32 +0300

    Update Parking.sol

--- contracts/Parking.sol
+++ contracts/Parking.sol
@@ -1,4 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Parking {}
+contract Parking {
+    struct PPM {
+        string name;
+        string entrance;
+    }
+
+    mapping(address => PPM[]) ppms;
+
+    function addPPM(string memory name) public {
+        ppms[msg.sender].push(PPM(name, "1"));
+    }
+}
